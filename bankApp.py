df7899b46a92f748820e98d3a877a94d7ce08d50
Rebecca Frank <rebeccaffrank@gmail.com>
2018-10-03 21:09:48 +0000

    Indentation fix

--- bankApp.py
+++ bankApp.py
@@ -11,15 +11,16 @@ def gatekeeper(login):
   if login == "admin":
     return "You have the privileges"
 
-# 3. Call the gatekeeper function with a string and print what it returns. 
+# 3. Call the gatekeeper function with a string and print what it returns.
 print(gatekeeper("admin"))
 
-# 4. How could this code be improved? Make it better. Think about what other scenarios you should cover in your if logic. 
+# 4. How could this code be improved? Make it better. Think about what other scenarios you should cover in your if logic.
 
 # 5. Complete the function called check_balance that takes one parameter, loan_balance.
 def check_balance(loan_balance):
-
 # 6. If loan_balance is zero or more, it says “you don’t owe any money”
 # 7. If loan_balance is negative, it  says “you owe $X” where X is the amount
+    return
+
 # 8. Call your function with a negative and positive number and print what it returns.
 print(check_balance(300))
